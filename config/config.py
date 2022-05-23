b2e24b514e7db2d7082bb2808eddbdef729ce1d9
Dave Barragan <itsdaveba@gmail.com>
2022-05-23 14:42:31 -0500

    documentation

--- config/config.py
+++ config/config.py
@@ -5,14 +5,12 @@ BASE_PATH = Path(__file__).parent.parent.resolve()
 CONFIG_DIR = Path(BASE_PATH, 'config')
 DATA_DIR = Path(BASE_PATH, 'data')
 TEST_DIR = Path(BASE_PATH, 'tests')
-BLOB_DIR = Path(BASE_PATH, 'blob')
 MODEL_DIR = Path(BASE_PATH, 'models')
 
 # Create directories
 CONFIG_DIR.mkdir(exist_ok=True)
 DATA_DIR.mkdir(exist_ok=True)
 TEST_DIR.mkdir(exist_ok=True)
-BLOB_DIR.mkdir(exist_ok=True)
 MODEL_DIR.mkdir(exist_ok=True)
 
 # Files
