e1c6089286562c94b7449531a3d4f0bf5a2328dc
Dave <itsdaveba@gmail.com>
2022-05-31 06:36:39 -0500

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,4 +6,5 @@ source venv/bin/activate
 python -m pip install --upgrade pip setuptools wheel
 python -m pip install dvc[gdrive]==2.10.2
 python -m pip install -r api-requirements.txt
+dvc init --no-scm
 dvc pull
