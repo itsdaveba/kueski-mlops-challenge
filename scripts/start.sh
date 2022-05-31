a5ffbb95e91bf45611d4b720bcd9b46c63c0a9be
Dave <itsdaveba@gmail.com>
2022-05-31 10:42:05 -0500

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,5 +4,4 @@ cd /home/ec2-user/mlops
 source venv/bin/activate
 dvc pull data/api_dataset.pkl
 dvc pull models/api.joblib
-docker build -t mlops:latest .
-docker run -d -p 5000:80 --name mlops mlops:latest
+supervisord -c supervisord.conf
