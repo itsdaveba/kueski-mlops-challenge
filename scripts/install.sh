f6009e39f1e7e3f09199fd31140c51cf30b3fa92
Dave <itsdaveba@gmail.com>
2022-05-30 23:47:50 -0500

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,5 @@ cd /home/ec2-user/mlops
 python3 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip setuptools wheel
-python -m pip install -r api-requirements.txt
+python -m pip install -r requirements.txt
+dvc pull
