91b54046b278047f86b7f55585e75bc71c373dab
Dave <itsdaveba@gmail.com>
2022-05-31 06:33:07 -0500

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,6 @@ cd /home/ec2-user/mlops
 python3 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip setuptools wheel
-python -m pip install -r requirements.txt
+python -m pip install dvc[gdrive]==2.10.2
+python -m pip install -r api-requirements.txt
 dvc pull
