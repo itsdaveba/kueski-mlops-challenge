9c0e61bc47de33070fb7c02ad85e8a7d60e8d0ca
Dave <itsdaveba@gmail.com>
2022-05-31 09:57:22 -0500

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,4 +4,5 @@ cd /home/ec2-user/mlops
 source venv/bin/activate
 dvc pull data/api_dataset.pkl
 dvc pull models/api.joblib
-uvicorn app.api:app --port 5000
+docker build -t mlops:latest .
+docker run -d -p 5000:80 --name mlops mlops:latest
