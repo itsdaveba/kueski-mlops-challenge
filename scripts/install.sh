b9e322a537cf10fe793166706c3e9bd9ad7562d3
Dave <itsdaveba@gmail.com>
2022-05-31 06:45:35 -0500

    test

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd /home/ec2-user/mlops
-python3 -m venv venv
-source venv/bin/activate
-python -m pip install --upgrade pip setuptools wheel
-python -m pip install dvc[gdrive]==2.10.2
-python -m pip install -r api-requirements.txt
-dvc init --no-scm
-dvc pull
+#cd /home/ec2-user/mlops
+#python3 -m venv venv
+#source venv/bin/activate
+#python -m pip install --upgrade pip setuptools wheel
+#python -m pip install dvc[gdrive]==2.10.2
+#python -m pip install -r api-requirements.txt
+#dvc init --no-scm
+#dvc pull
